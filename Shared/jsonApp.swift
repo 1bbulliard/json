a025f9d944f6483b72d30393900a71a63d9034cc
bbulliard <bbulliard@gmail.com>
2021-10-29 14:00:34 -0500

    Initial Commit

---
+++ Shared/jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonApp.swift
+//  Shared
+//
+//  Created by Bob Bulliard on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
